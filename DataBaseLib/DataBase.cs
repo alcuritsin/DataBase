51529fbe455900209909e7dc4c60d5cb1e42fec7
Starinin Andrey <starininandrey@gmail.com>
2021-09-17 17:12:29 +0300

    Add DeserializeJson

--- DataBaseLib/DataBase.cs
+++ DataBaseLib/DataBase.cs
@@ -1,7 +1,66 @@
-﻿namespace DataBaseLib
+﻿using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+using static System.Text.Json.JsonSerializer;
+
+namespace DataBaseLib
 {
     public class DataBase
     {
+        #region Values
+
+        private MySqlConnection _db;
+        private MySqlCommand _command;
+        private DataBaseConnection _connection;
+
+        #endregion
+
+        #region Constructor
+
+        public DataBase()
+        {
+            _db = new MySqlConnection();
+            _command = new MySqlCommand();
+        }
+
+        #endregion
+
+        #region Init
+
+        private bool DeserializeJson(string path)
+        {
+            try
+            {
+                using var file = new FileStream(path, FileMode.Open);
+                _connection = DeserializeAsync<DataBaseConnection>(file).Result;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Init(string path)
+        {
+            var res = DeserializeJson(path);
+
+            if (!res) return false;
+            _db.ConnectionString = _connection.ToString();
+            return true;
+        }
+        
+        public bool Init()
+        {
+            var res = DeserializeJson("db_connection.json");
+
+            if (!res) return false;
+            _db.ConnectionString = _connection.ToString();
+            return true;
+        }
+
+        #endregion
         
     }
 }
---
+++ DataBaseLib/DataBaseConnection.cs
@@ -0,0 +1,16 @@
+﻿namespace DataBaseLib
+{
+    public class DataBaseConnection
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Uid { get; set; }
+        public string Pwd { get; set; }
+
+        public override string ToString()
+        {
+            //Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+            return $"Server={Server};Database={Database};Uid={Uid};Pwd={Pwd};";
+        }
+    }
+}
